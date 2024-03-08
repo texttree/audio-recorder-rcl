ab86a7587c80df88622a8c2dd0a707890b8b630e
Vitek Foxprogs <foxprogs@gmail.com>
2024-03-08 17:01:37 +0400

    fix: added missed file

--- src/components/Audio/index.js
+++ src/components/Audio/index.js
@@ -1,4 +1,5 @@
 export { default as AudioPlayer } from './AudioPlayer';
 export { default as AudioRecorder } from './AudioRecorder';
+export { default as AudioMarkup } from './AudioMarkup';
 export { default as AudioRecorderTranscriber } from './AudioRecorderTranscriber';
 export { default as AudioEditor } from './AudioEditor';
