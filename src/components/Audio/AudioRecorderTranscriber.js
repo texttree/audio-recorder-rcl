262229f14d4c3078dc22b4601264ed222f139ddf
Vitek Foxprogs <foxprogs@gmail.com>
2024-03-08 16:43:14 +0400

    feat: :memo: add docs

--- src/components/Audio/AudioRecorderTranscriber.js
+++ src/components/Audio/AudioRecorderTranscriber.js
@@ -7,13 +7,14 @@ import VoiceToText from '@texttree/voice2text';
 const AudioRecorderTranscriber = () => {
   const v2t = useRef();
   const waveformRef = useRef(null);
-  const recordingsRef = useRef(null);
+  const recordingRef = useRef(null);
   const progressRef = useRef(null);
   const [time, setTime] = useState('00:00');
   const [isDisabled, setIsDisabled] = useState(true);
-  const [transcribedText, setTranscribedText] = useState('');
+  const [transcribedText, setTranscribedText] = useState([]);
   const [tempTranscribedText, setTempTranscribedText] = useState('');
   const [wavesurfer, setWaveSurfer] = useState(null);
+  const resultWavesurfer = useRef(null);
   const [record, setRecord] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
   const [isPausedRecording, setIsPausedRecording] = useState(false);
@@ -52,7 +53,7 @@ const AudioRecorderTranscriber = () => {
       if (wavesurfer) {
         wavesurfer.destroy();
       }
-      const newWaveSurfer = WaveSurfer.create({
+      const newRecordWaveSurfer = WaveSurfer.create({
         container: waveformRef.current,
         waveColor: 'rgb(200, 0, 200)',
         progressColor: 'rgb(100, 0, 100)',
@@ -66,26 +67,32 @@ const AudioRecorderTranscriber = () => {
         ],
       });
 
-      const newRecord = newWaveSurfer.registerPlugin(
+      const newRecord = newRecordWaveSurfer.registerPlugin(
         RecordPlugin.create({ scrollingWaveform: true, renderRecordedAudio: true })
       );
 
       newRecord.on('record-end', (blob) => {
-        const container = recordingsRef.current;
+        if (resultWavesurfer?.current) {
+          resultWavesurfer.current.destroy();
+        }
+        const container = recordingRef.current;
+        container.innerHTML = '';
         const recordedUrl = URL.createObjectURL(blob);
 
-        const newWaveSurfer = WaveSurfer.create({
+        resultWavesurfer.current = WaveSurfer.create({
           container,
           waveColor: 'rgb(200, 100, 0)',
           progressColor: 'rgb(100, 50, 0)',
           url: recordedUrl,
+          interact: true,
+          dragToSeek: true,
         });
 
         const button = document.createElement('button');
         button.textContent = 'Play';
-        button.onclick = () => newWaveSurfer.playPause();
-        newWaveSurfer.on('pause', () => (button.textContent = 'Play'));
-        newWaveSurfer.on('play', () => (button.textContent = 'Pause'));
+        button.onclick = () => resultWavesurfer.current.playPause();
+        resultWavesurfer.current.on('pause', () => (button.textContent = 'Play'));
+        resultWavesurfer.current.on('play', () => (button.textContent = 'Pause'));
         container.appendChild(button);
         const link = document.createElement('a');
         Object.assign(link, {
@@ -98,7 +105,7 @@ const AudioRecorderTranscriber = () => {
       newRecord.on('record-progress', (time) => {
         updateProgress(time);
       });
-      setWaveSurfer(newWaveSurfer);
+      setWaveSurfer(newRecordWaveSurfer);
       setRecord(newRecord);
     };
 
@@ -112,7 +119,7 @@ const AudioRecorderTranscriber = () => {
     voice2text.init();
     v2t.current = voice2text;
     // Listen to the result event
-    window.addEventListener('voice', (e) => {
+    const parseEvent = (e) => {
       console.log(e.detail);
       switch (e.detail.type) {
         case 'PARTIAL':
@@ -123,20 +130,28 @@ const AudioRecorderTranscriber = () => {
           break;
         case 'FINAL':
           setTempTranscribedText('');
-          setTranscribedText((prev) => prev + ' ' + e.detail.text);
+          setTranscribedText((prev) => prev.concat(e.detail.result));
           break;
         default:
           break;
       }
-    });
+    };
+    window.addEventListener('voice', parseEvent);
 
     return () => {
       if (wavesurfer) {
         wavesurfer.destroy();
       }
-      window.removeEventListener('voice');
+      window.removeEventListener('voice', parseEvent);
     };
   }, []);
+
+  const setResultTime = (time) => {
+    console.log(time, resultWavesurfer);
+    if (resultWavesurfer.current) {
+      resultWavesurfer.current.setTime(time);
+    }
+  };
 
   const updateProgress = (time) => {
     const formattedTime = [
@@ -171,10 +186,42 @@ const AudioRecorderTranscriber = () => {
         }}
       ></div>
       <div>
-        {transcribedText} {tempTranscribedText}
+        <TranscribeBlock transcribedText={transcribedText} setTime={setResultTime} />{' '}
+        {tempTranscribedText}
       </div>
-      <div ref={recordingsRef} style={{ margin: '1rem 0' }}></div>
+      <div ref={recordingRef} style={{ margin: '1rem 0' }}></div>
     </div>
   );
 };
+
+const TranscribeBlock = ({ transcribedText, setTime }) => (
+  <div
+    style={{
+      border: '1px solid #ddd',
+      borderRadius: '4px',
+      marginTop: '1rem',
+      overflow: 'auto',
+    }}
+  >
+    <span>
+      {transcribedText.map((word, idx) => (
+        <Word
+          key={idx}
+          word={word.word}
+          conf={word.conf}
+          start={word.start}
+          end={word.end}
+          onClick={() => setTime(word.start)}
+        />
+      ))}
+    </span>
+  </div>
+);
+
+const Word = ({ conf, end, start, word, onClick }) => (
+  <span style={{ color: conf < 0.7 ? '#999' : '#000' }} title={conf} onClick={onClick}>
+    {word}{' '}
+  </span>
+);
+
 export default AudioRecorderTranscriber;
